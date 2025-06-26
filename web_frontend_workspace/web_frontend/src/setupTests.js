502579819bf23e24d64f7b58219d47040172f707
Kavia AI <production@kavia.ai>
2025-06-26 10:05:57 +0000

    CheckPoint

--- web_frontend_workspace/web_frontend/src/setupTests.js
+++ web_frontend_workspace/web_frontend/src/setupTests.js
@@ -3,3 +3,22 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock scrollIntoView (jsdom does not implement it)
+if (typeof window.HTMLElement !== "undefined" && !window.HTMLElement.prototype.scrollIntoView) {
+  window.HTMLElement.prototype.scrollIntoView = function() {};
+}
+
+// Mock window.matchMedia for theme and responsive tests
+if (typeof window.matchMedia !== "function") {
+  window.matchMedia = function() {
+    return {
+      matches: false,
+      addEventListener: function() {},
+      removeEventListener: function() {},
+      addListener: function() {},
+      removeListener: function() {},
+      dispatchEvent: function() {},
+    };
+  };
+}
